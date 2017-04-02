a631994d33d7d06dae1c00400462209c6adffc6d
Henri Romel <henri@localhost.localdomain>
2017-04-02 15:41:05 -0400

    update

--- utils.py
+++ utils.py
@@ -1,52 +1,136 @@
 from mido import MidiFile, MidiTrack, Message
 import os
 
+# Check Equal: Assert that 2 arguments are equal. If they are not raise an exception with a error type
+def chEq(x, y, error_type):
+	if x != y:
+		message = "{} error: {} != {}".format(error_type, x, y)
+		raise Exception(message)
 
-def midiToNoteArray(file_name):
-	mid = MidiFile(file_name) # a Mozart piece
+# Check Greater than or Equal to
+def chGe(x, y, error_type):
+	if x < y:
+		message = "{} error: {} < {}".format(error_type, x, y)
+		raise Exception(message)
 
-	notes = []
+def chNotIn(val, my_collection, error_type):
+	if val in my_collection:
+		message = "{} error: {} in {}".format(error_type, val, my_collection)
+		raise Exception(message)
 
-	time = float(0)
-	prev = float(0)
+def chIn(val, my_collection, error_type):
+	if val not in my_collection:
+		message = "{} error: {} not in {}".format(error_type, val, my_collection)
+		raise Exception(message)
 
-	for msg in mid:
-		#print msg
 
-		### this time is in seconds, not ticks
-		time += msg.time
-		if not msg.is_meta:
-			### only interested in piano channel
-			if msg.channel == 1:
-				if msg.type == 'note_on':
-					# note in vector form to train on
-					data = msg.bytes() 
-					# only interested in the note and velocity. note message is in the form of [type, note, velocity]
-					note, velocity = data[1:3]
-					notes.append([note, time-prev])
-					prev = time
 
-	return notes
+class Key():
+	def __init__(self, tonic, type):
+		# ....
+	def getLhChord():
+
+	def getMiddleTonic():
+
+class Melody():
+	def __init__(self, ppqn):
+		self.ppqn = ppqn
+		self.notes = []
+	def addNote(self, note_midi, start_time, end_time):
+		notes.append(note_midi, start_time, end_time)
+
+
+class Song():
+	def __init__(self):
+		# epsilon
+		self.melody = []
+		self.harmony = []
+		self.tempo = 120
+		self.time_sig = [4, 4]
+		self.key_sig = 'C'
+		self.instrument = 1 # always piano
+	def loadMidi(self, file_name):
+		mid = MidiFile(file_name)
+		ppqn = mid.ticks_per_beat # ie pulses per quarter note
+		acc_time = 0
+
+		chGe(len(mid.tracks), 2, "n_tracks")
+
+		# Melody
+		melody = Melody(ppqn)
+		acc_time = 0
+		track = mid.tracks[0]
+		note_starts = {}
+
+		for msg in track:
+			if msg.type == 'note_off' or (msg.type == 'note_on' and msg.veloctiy==0):
+				# (note_off is equivalent to note_on with 0 velocity)
+				# (note that midi files can play notes then leave a brief rest before the next note
+				#  which isn't explicitly marked as a rest)
+				#print msg.velocity, msg.note, msg.time
+
+				acc_time += msg.time
+				chNotIn(msg.note, note_starts.keys(), "note")
+
+				mel.addNote(msg.note, note_starts[msg.note], acc_time)
+
+				del note_starts[msg.note]
+
+
+			elif msg.type == 'note_on':
+				#print msg.velocity, msg.note, msg.time
+				# default: velocity 64, otherwise 0
+				# note: between 0 and 127 inclusively where 60 is middle C (40th note on piano),
+				#       A0 is 21, and C8 is 108
+				# time: specifies how much you wait before playing this note next
+				#       eg. time=0 means you play it immediately as you get the message
+				#       the units are ticks
+
+				acc_time += msg.time
+				chIn(msg.note, note_starts.keys(), "note")
+				note_starts[msg.note] == acc_time
+
+
+				#elif msg.type == 'program_change':
+				#print msg.program # instrument
+				# 1: acoustic grand piano
+				# 2-6: other piano types
+				#print msg.channel
+				# channel where the instrument is used (10 is supposed to be for precussion?)
+
+			elif msg.type == 'key_signature':
+				#print msg.key_signature
+				# can be 'Dbm' for D flat minor
+				# default 'C' for C major
+				self.key_sig = msg.key_signature
+
+			elif msg.type == 'time_signature':
+				#print msg.numerator, msg.denominator, msg.clocks_per_click
+				#print msg.notated_32nd_notes_per_beat
+				# default 4, 4, 24, 8
+
+				# I'm not sure how to handle other clocks_per_click and notated_32nd_notes_per_beat values
+				chEq(msg.clocks_per_click, 24, "clocks_per_tick")
+				chEq(msg.notated_32nd_notes_per_beat, 8, "notated_32nd_notes_per_beat")
+
+				self.time_sig = [msg.numerator, msg.denominator]
+
+			elif msg.type == 'set_tempo':
+				#print msg.set_tempo
+				# possible: 0 ... 16777215
+				# default: 500 000
+				# this means 500 000 microseconds (0.5 s) per beat (quarter note)
+				# thus the default is 120 BMP (beats per minute)
+				self.tempo = int((60 * 1000 * 1000) / tempo)
+
+		chEq(len(notes.keys()), 0, "note_dictionary_empty")
+		
+		return notes
 
 def noteArrayToMidi(note_array, file_name):
 	mid = MidiFile()
-	track = MidiTrack()
-	mid.tracks.append(track)
-	track.append(Message('program_change', program=12, time=0))
+	
 
-	for note, time in note_array:
-		# 147 means note_on
-		#note = np.insert(note, 0, 147)
-		#bytes = note.astype(int)
-		time /= 0.001025
-		time = int(time)
-		track.append(Message('note_on', note=note, velocity=100, time=time))
-		track.append(Message('note_off', note=note, velocity=100, time=time))
-		#print (note)
-		#msg = Message.from_bytes(bytes[0:3]) 
-		#time = int(note[3]/0.001025) # to rescale to midi's delta ticks. arbitrary value for now.
-		#msg.time = time
-		#track.append(msg)
 
 	mid.save(file_name)
 
@@ -59,6 +143,5 @@ if __name__ == "__main__":
 		base_name = os.path.basename(f)
 		output_path = os.path.join("Generated/", base_name)
 		noteArrayToMidi(arr, output_path)
-		break
 
 	print arr
---
+++ utils3.py
@@ -0,0 +1,64 @@
+from mido import MidiFile, MidiTrack, Message
+import os
+
+
+def midiToNoteArray(file_name):
+	mid = MidiFile(file_name) # a Mozart piece
+
+	notes = []
+
+	time = float(0)
+	prev = float(0)
+
+	for msg in mid:
+		#print msg
+
+		### this time is in seconds, not ticks
+		time += msg.time
+		if not msg.is_meta:
+			### only interested in piano channel
+			if msg.channel == 1:
+				if msg.type == 'note_on':
+					# note in vector form to train on
+					data = msg.bytes() 
+					# only interested in the note and velocity. note message is in the form of [type, note, velocity]
+					note, velocity = data[1:3]
+					notes.append([note, time-prev])
+					prev = time
+
+	return notes
+
+def noteArrayToMidi(note_array, file_name):
+	mid = MidiFile()
+	track = MidiTrack()
+	track.append(Message('program_change', program=12, time=0))
+	mid.tracks.append(track)
+
+	for note, time in note_array:
+		# 147 means note_on
+		#note = np.insert(note, 0, 147)
+		#bytes = note.astype(int)
+		#time /= 0.001025
+		time /= 0.002050
+		time = int(time)
+		track.append(Message('note_on', note=note, velocity=100, time=time))
+		track.append(Message('note_off', note=note, velocity=100, time=time))
+		#print (note)
+		#msg = Message.from_bytes(bytes[0:3]) 
+		#time = int(note[3]/0.001025) # to rescale to midi's delta ticks. arbitrary value for now.
+		#msg.time = time
+		#track.append(msg)
+
+	mid.save(file_name)
+
+if __name__ == "__main__":
+	mypath = "siraj-music"
+	siraj_files = [os.path.join(mypath, f) for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+	siraj_files = filter(lambda x: x.endswith(".midi"), siraj_files)
+	for f in siraj_files:
+		arr = midiToNoteArray(f)
+		base_name = os.path.basename(f)
+		output_path = os.path.join("Generated/", base_name)
+		noteArrayToMidi(arr, output_path)
+
+	print arr
