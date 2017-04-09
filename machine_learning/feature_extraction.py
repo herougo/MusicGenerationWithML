42cde51dddeb390f446ec25645d84a6f4db4c0b1
Henri Romel <henri@localhost.localdomain>
2017-04-08 20:15:37 -0400

    feature extraction start

---
+++ machine_learning/feature_extraction.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+# Assume we have array of size 5*4*16 = 320 sixteenth note beats
+def extractFeatures(sixteenth_arr):
+	pass
+
+
+def filterSixteenthArr(sixteenth_arr):
+	pass
+
+# ###################################
+# Helper Functions ##################
+# ###################################
+
+
+
+def totalMelodyDistance()
+
+
+
+
+
+
+
+
+
+
+
+''' Planned Features
+total melody distance
+total distance of leaps
+number of notes fitting chord
+number of non-chord tones
+proportion of chord-fitting notes
+uses minor 3-2-1
+uses minor 6-5
+minor high distance low
+non-leap percentage
+last note in key
+long notes in key
+long note tonic (or 3rd) of key
+percentage of non-chord tones that resolve stepwise to inside the chord
+percentage of non-chord notes which came stepwise from a chord tone
+longest break between notes
+last bar in phrase
+is tonic (or 3rd or 5th) of current chord reached
+is IV chord
+# changes in direction
+max interval
+count 7th of chord as part of chord
+number of distinct pitches (ie high C and low C count as different pitches))
+Skew of sorted pitch distribution of used pitches
+Proportion in chord
+Dot product of intervals and proportion
+Final note is tonic
+Interval between next note divided by current note duration
+proportion of notes starting on the quarter note or eight note
+Median of interval sizes
+321, 31, 43, 65, proportion of 7, 1, 3, 4
+'''
